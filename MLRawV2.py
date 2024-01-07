486daa980549ede4b5ac15dd87062c501a877e8d
R-Mchi <86179471+R-Mchi@users.noreply.github.com>
2024-01-07 20:51:47 +0700

    Update MLRawV2.py

--- MLRawV2.py
+++ MLRawV2.py
@@ -1,25 +1,27 @@
 import cv2
 import mediapipe as mp
 
-# Inisialisasi MediaPipe Pose
 mp_pose = mp.solutions.pose
-pose = mp_pose.Pose()
+pose = mp_pose.Pose(static_image_mode=False, model_complexity=1, enable_segmentation=False, min_detection_confidence=0.5)
 
-# Membuka video atau kamera
 user_choice = input("Tekan 'c' untuk kamera atau 'f' untuk file video: ").lower()
 if user_choice == 'f':  
-    path_to_file = 'data/evan2.mp4'
+    path_to_file = 'data/Fikra2.mp4'
     cap = cv2.VideoCapture(path_to_file)
 elif user_choice == 'c':
     cap = cv2.VideoCapture(0)
 
-# Menentukan frame rate
+th_take_off = 0.03
+th_take_landing = 0.035
+
 frame_rate = cap.get(cv2.CAP_PROP_FPS)
 frame_count = 0
 ankle_reference = None
 take_off_time = None
 landing_time = None
 flight_time = 0
+is_in_air = False  
+highest_jump = 0
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -30,31 +32,37 @@ while cap.isOpened():
     results = pose.process(frame_rgb)
 
     if results.pose_landmarks:
-        ankle_current = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ANKLE]
+        ankle_left_current = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ANKLE]
+        ankle_right_current = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ANKLE]
 
         if frame_count == 0:
-            # Mengambil posisi ankle pada frame pertama sebagai referensi
-            ankle_reference = ankle_current
+            ankle_left_reference = ankle_left_current
+            ankle_right_reference = ankle_right_current
 
-        if ankle_reference:
+        if ankle_left_reference and ankle_right_reference:
             # Deteksi take-off
-            take_off = ankle_current.y - ankle_reference.y
-            if abs(take_off) > 0.05 and take_off_time is None:
+            if not is_in_air and (abs(ankle_left_current.y - ankle_left_reference.y) > th_take_off or 
+                                  abs(ankle_right_current.y - ankle_right_reference.y) > th_take_off):
+                #print(ankle_current.y - ankle_reference.y)
                 take_off_time = frame_count / frame_rate
-                print(f"Take-off detected at: {take_off_time:.2f}s")
+                is_in_air = True
+                #print(f"Take-off detected at: {take_off_time:.4f}s")
 
-            # Deteksi landing
-            if abs(ankle_current.y - ankle_reference.y) < 0.05 and take_off_time is not None:
+            if is_in_air and (abs(ankle_left_current.y - ankle_left_reference.y) < th_take_landing and 
+                              abs(ankle_right_current.y - ankle_right_reference.y) < th_take_landing):
                 landing_time = frame_count / frame_rate
                 flight_time = landing_time - take_off_time
-                print(f"Landing detected at: {landing_time:.2f}s")
-                print(f"Flight time: {flight_time:.2f}s")
+                is_in_air = False  
+                #print(f"Landing detected at: {landing_time:.4f}s")
+                #print(f"Flight time: {flight_time:.4f}s")
 
-                # Hitung tinggi lompatan menggunakan flight time
-                height_jump = (flight_time)** 2 * 1.22625
-                print(f"Tinggi lompatan: {height_jump:.2f} meter")
+                height_jump = (flight_time** 2) * 1.22625 * 100
+                if height_jump > highest_jump:
+                    highest_jump = height_jump
+                
 
-                # Reset take-off dan landing time untuk deteksi lompatan berikutnya
+                ankle_left_reference = ankle_left_current
+                ankle_right_reference = ankle_right_current
                 take_off_time = None
                 landing_time = None
 
@@ -64,5 +72,6 @@ while cap.isOpened():
     if cv2.waitKey(5) & 0xFF == 27:
         break
 
+print(f"Tinggi lompatan: {highest_jump:.4f} cm")
 cap.release()
 cv2.destroyAllWindows()
